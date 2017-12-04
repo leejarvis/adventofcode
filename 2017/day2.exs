00ae2a1107e01a292ace9071c34ba4d2c8b645eb
Lee Jarvis <leejarvis@fastmail.com>
2017-12-04 15:23:41 +0000

    Add 2017/day2.exs

---
+++ 2017/day2.exs
@@ -0,0 +1,44 @@
+# http://adventofcode.com/2017/day/2
+
+defmodule Day2 do
+  def checksum(content) do
+    content
+    |> parse
+    |> calc_diff
+    |> sum
+  end
+
+  def sum(rows) do
+    Enum.reduce(rows, 0, fn({_, x}, acc) -> x + acc end)
+  end
+
+  defp calc_diff(rows) do
+    Enum.map(rows, fn(r) ->
+      {min, max} = Enum.min_max(r)
+      {r, max - min}
+    end)
+  end
+
+  defp parse(content) do
+    content
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+end
+
+str = """
+5 1 9 5
+7 5 3
+2 4 6 8
+"""
+
+str
+|> Day2.checksum
+|> IO.inspect
