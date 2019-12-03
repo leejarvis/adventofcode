ac29cb91871ec72b426b56f6b4936a20ae8f3da1
Lee Jarvis <leejarvis@fastmail.com>
2019-12-03 15:38:57 +0000

    Shorten 2019/day2

--- 2019/day2.rb
+++ 2019/day2.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 
 i = File.read("res/day2.txt").split(",").map(&:to_i)
-k,i[1],i[2],c=0,12,2
-c=i[k]
-while c != 99
-  c=i[k+=i[k+1,3].tap{|l,r,x|i[x]=i[l].send(c==1 ? :+ : :*,i[r])}.size+1]
-end
+i[1],i[2]=12,2
+i.each_slice(4){|d,k,v,o|d==99 ? break : i[o]=i[k].send(d==1 ? :+ : :*,i[v])}
 p i[0]
