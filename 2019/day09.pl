2901541fbe14edc2fafa306684b3d36d544791d1
Lee Jarvis <leejarvis@fastmail.com>
2019-12-09 19:18:33 +0000

    Tweak 2019/day 9
    Simplify code a bit and remove warnings (due to.. introducing some
    warnings)

--- 2019/day09.pl
+++ 2019/day09.pl
@@ -1,88 +1,36 @@
 #!/usr/bin/perl -l
 
-use strict;
-use warnings;
-
 sub execute {
-  my $instructions = shift;
-  my @instructions = @{$instructions};
-
-  my $input = shift;
+  my ($instructions, $input) = @_;
+  my @ins = @{$instructions};
   my @inputs = ($input);
+  my (@shifts, $pos, $rel) = ((0, 4, 4, 2, 2, 3, 3, 4, 4, 2), 0, 0);
 
-  my $pos = 0;
-  my $relative = 0;
+  while ((my $op = $ins[$pos]) != 99) {
+    my ($p1, $p2, $p3) = map { ($ins[$pos + $_], $pos + $_, $ins[$pos + $_] + $rel)[int($op / 10 ** ($_ + 1)) % 10] }(1..3);
 
-  while ($instructions[$pos] != 99) {
-    my $op = $instructions[$pos];
-    my ($p1, $p2, $p3);
+    $op = $op % 100;
+    $pos += $shifts[$op];
 
-    my $p1mode = int($op / 100) % 10;
-    my $p2mode = int($op / 1000) % 10;
-    my $p3mode = int($op / 10000) % 10;
+    my %subs = (
+      1 => sub { $ins[$p3] = $ins[$p1] + $ins[$p2] },
+      2 => sub { $ins[$p3] = $ins[$p1] * $ins[$p2] },
+      3 => sub { $ins[$p1] = shift(@inputs) },
+      4 => sub { push(@inputs, $ins[$p1]) },
+      5 => sub { $pos = $ins[$p2] if $ins[$p1] != 0 },
+      6 => sub { $pos = $ins[$p2] if $ins[$p1] == 0 },
+      7 => sub { $ins[$p3] = ($ins[$p1] < $ins[$p2]) ? 1 : 0 },
+      8 => sub { $ins[$p3] = ($ins[$p1] == $ins[$p2]) ? 1 : 0 },
+      9 => sub { $rel += $ins[$p1] }
+    );
 
-    if ($p1mode == 0) {
-      $p1 = $instructions[$pos + 1];
-    } elsif ($p1mode == 1) {
-      $p1 = $pos + 1
-    } elsif ($p1mode == 2) {
-      $p1 = $instructions[$pos + 1] + $relative;
-    }
-
-    if ($p2mode == 0) {
-      $p2 = $instructions[$pos + 2];
-    } elsif ($p2mode == 1) {
-      $p2 = $pos + 2
-    } elsif ($p2mode == 2) {
-      $p2 = $instructions[$pos + 2] + $relative;
-    }
-
-    if ($p3mode == 0) {
-      $p3 = $instructions[$pos + 3];
-    } elsif ($p3mode == 1) {
-      $p3 = $pos + 3
-    } elsif ($p3mode == 2) {
-      $p3 = $instructions[$pos + 3] + $relative;
-    }
-
-    $op = substr("$op", -1);
-
-    if ($op == "1") {
-      $instructions[$p3] = $instructions[$p1] + $instructions[$p2];
-      $pos += 4;
-    } elsif ($op == "2") {
-      $instructions[$p3] = $instructions[$p1] * $instructions[$p2];
-      $pos += 4;
-    } elsif ($op == "3") {
-      $instructions[$p1] = shift(@inputs);
-      $pos += 2;
-    } elsif ($op == "4") {
-      push(@inputs, $instructions[$p1]);
-      $pos += 2;
-    } elsif ($op == "5") {
-      ($instructions[$p1] == 0) ? ($pos += 3) : ($pos = $instructions[$p2]);
-    } elsif ($op == "6") {
-      ($instructions[$p1] == 0) ? ($pos = $instructions[$p2]) : ($pos += 3);
-    } elsif ($op == "7") {
-      $instructions[$p3] = ($instructions[$p1] < $instructions[$p2]) ? 1 : 0;
-      $pos += 4;
-    } elsif ($op == "8") {
-      $instructions[$p3] = ($instructions[$p1] == $instructions[$p2]) ? 1 : 0;
-      $pos += 4;
-    } elsif ($op == "9") {
-      $relative += $instructions[$p1];
-      $pos += 2;
-    } else {
-      die "Unknown op `$op`";
-    }
+    %subs{$op}->();
   }
 
   return @inputs;
 }
 
-open(FH, "res/day9.txt");
-my @instructions = map { int($_) } split(/,/, <FH>);
-close(FH);
+my @instructions = split(/,/, do { local(@ARGV, $/) = "res/day9.txt" ; <>});
 
 print execute(\@instructions, 1);
 print execute(\@instructions, 2);
