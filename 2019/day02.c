f0261acc63a4e874849339468149a496b080f3af
Lee Jarvis <leejarvis@fastmail.com>
2019-12-19 17:29:40 +0000

    Add day 2 part 2

--- 2019/day02.c
+++ 2019/day02.c
@@ -1,47 +1,76 @@
-// gcc day2.c && ./a.out && rm a.out
+// gcc day02.c && ./a.out && rm a.out
 
 #include <stdio.h>
 
 #define LEN 149
-int instructions[LEN];
+int program[LEN];
 
 void read_input() {
     FILE *fh = fopen("res/day2.txt", "r");
     for (int i = 0; i < LEN; i++) {
-        fscanf(fh, "%d%*c", &instructions[i]);
+        fscanf(fh, "%d%*c", &program[i]);
     }
     fclose(fh);
 }
 
-int main() {
-    read_input();
-
-    instructions[1] = 12;
-    instructions[2] = 2;
-
+void execute(int *program) {
     int pos = 0;
-    int op = instructions[pos];
+    int op = program[pos];
 
     while (op != 99) {
-        int a1 = instructions[pos + 1];
-        int v1 = instructions[a1];
+        int a1 = program[pos + 1];
+        int v1 = program[a1];
 
-        int a2 = instructions[pos + 2];
-        int v2 = instructions[a2];
+        int a2 = program[pos + 2];
+        int v2 = program[a2];
 
-        int loc = instructions[pos + 3];
+        int loc = program[pos + 3];
 
         if (op == 1) {
-            instructions[loc] = v1 + v2;
+            program[loc] = v1 + v2;
         } else if (op == 2) {
-            instructions[loc] = v1 * v2;
+            program[loc] = v1 * v2;
         }
 
         pos += 4;
-        op = instructions[pos];
+        op = program[pos];
     }
+}
 
-    printf("%d\n", instructions[0]);
+void part1() {
+    program[1] = 12;
+    program[2] = 2;
+
+    execute(program);
+
+    printf("%d\n", program[0]);
+}
+
+void part2() {
+    read_input();
+
+    for (int noun = 0; noun < 1000; noun++) {
+        for (int verb = 0; verb < 1000; verb++) {
+            read_input();
+
+            program[1] = noun;
+            program[2] = verb;
+
+            execute(program);
+
+            if (program[0] == 19690720) {
+                printf("%d%d\n", noun, verb);
+                return;
+            }
+        }
+    }
+}
+
+int main() {
+    read_input();
+    part1();
+
+    part2();
 
     return 0;
 }
