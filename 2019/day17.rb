1f820a2a71c9cb8fbddac34c89f1c1fd48d84dda
Lee Jarvis <leejarvis@fastmail.com>
2019-12-23 19:42:13 +0000

    Add 2019/day17 (part 1)

---
+++ 2019/day17.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+require_relative "intcode"
+
+class Grid
+  attr_reader :grid
+
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def at(row, col)
+    grid[row] && grid[row][col]
+  end
+
+  def each_with_index
+    grid.each_with_index do |row, rindex|
+      row.each_with_index do |col, cindex|
+        yield row, rindex, col, cindex
+      end
+    end
+  end
+
+  def intersections
+    o = []
+    each_with_index do |r, rx, c, cx|
+      if c == "#" &&
+        at(rx - 1, cx) == "#" &&
+        at(rx + 1, cx) == "#" &&
+        at(rx, cx + 1) == "#" &&
+        at(rx, cx - 1) == "#"
+
+        o << [rx, cx]
+      end
+    end
+    o
+  end
+end
+
+program = File.read("res/day17.txt").split(",").map(&:to_i)
+mapping = { "35" => "#", "46" => ".", "10" => "\n" }
+output = Intcode.run(program).join.gsub(Regexp.union(mapping.keys), mapping)
+
+grid = Grid.new(output.split("\n").map { |s| s.split("") })
+# grid = Grid.new(DATA.read.split("\n").map { |s| s.split("") })
+
+p grid.intersections.map { |top, left| top * left }.sum
+
+__END__
+..#..........
+..#..........
+#######...###
+#.#...#...#.#
+#############
+..#...#...#..
+..#####...^..
