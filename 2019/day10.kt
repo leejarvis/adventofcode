7f21e5dd8b7ecffe67b940e176fa3f5e756b245d
Lee Jarvis <leejarvis@fastmail.com>
2020-01-02 17:04:31 +0000

    Add 2019/day10 (part 2)
    This took way longer than I'll ever admit, and I just straight up refuse
    to clean up the Kotlin code. I did not enjoy this :|

--- 2019/day10.kt
+++ 2019/day10.kt
@@ -2,6 +2,7 @@
 
 import java.io.File
 import kotlin.math.atan2
+import kotlin.math.hypot
 
 fun main() {
     val asteroids = mutableListOf<Pair<Int, Int>>()
@@ -15,8 +16,7 @@ fun main() {
     }
 
     val detects = asteroids.map { point ->
-        asteroids
-            .filter { it != point }
+        asteroids.filter { it != point }
             .map {
                 val dx = it.first - point.first
                 val dy = it.second - point.second
@@ -27,4 +27,43 @@ fun main() {
     }
 
     println(detects.max())
+
+    val max = asteroids.maxBy { point ->
+        asteroids.filter { it != point }
+            .map {
+                val dx = it.first - point.first
+                val dy = it.second - point.second
+                atan2(dx.toDouble(), dy.toDouble())
+            }
+            .distinct()
+            .count()
+    }!!
+
+    val grouped = asteroids.groupBy {
+        val dx = it.first - max.first
+        val dy = it.second - max.second
+        atan2(dy.toDouble(), dx.toDouble())
+    }
+
+    var angle = -Math.PI / 2
+    var first = true
+    var result = asteroids.first()
+
+    (1..200).forEach {
+        val angles = grouped
+            .filter { if (first) it.key >= angle else it.key > angle }
+            .minBy { it.key }
+            ?: grouped.minBy { it.key }!!
+
+        angle = angles.key
+        first = false
+
+        result = angles.value.minBy {
+            val dx = it.first - max.first
+            val dy = it.second - max.second
+            hypot(dx.toDouble(), dy.toDouble())
+        }!!
+    }
+
+    println(result.first * 100 + result.second)
 }
