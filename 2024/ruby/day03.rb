9f6c9722064c343e9919e282c91afced453d1d1d
Lee Jarvis <lee@jrvs.uk>
2024-12-30 19:57:59 +0000

    2024/day03.rb

--- 2024/ruby/day03.rb
+++ 2024/ruby/day03.rb
@@ -1,8 +1,23 @@
 #!/usr/bin/env ruby
 
-parts = DATA.scan(/mul\((\d+),(\d+)\)/)
+parts = DATA.read.scan(/(mul\((\d+),(\d+)\)|don't\(\)|do\(\))/)
+p1 = p2 = 0
+enabled = true
 
-p parts
+parts.each { |(m, a, b)|
+  if m == "don't()"
+    enabled = false
+  elsif m == "do()"
+    enabled = true
+  else
+    sum = a.to_i * b.to_i
+    p1 += sum
+    p2 += sum if enabled
+  end
+}
+
+puts "P1: #{p1}"
+puts "P2: #{p2}"
 
 __END__
-xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
